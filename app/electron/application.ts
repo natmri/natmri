a82216cc6a48e82dcb2803ed7e11ecd2389778b0
starknt <1431880400@qq.com>
2022-12-09 15:43:06 +0800

    refactor(app/electron): migrate to `app/framework`

--- app/electron/application.ts
+++ app/electron/application.ts
@@ -1,8 +1,13 @@
-import type { IFrameApplicationConfiguration } from '@app/framework'
+import { join } from 'path'
+import type { IFrameApplicationConfiguration, IFrameApplicationLifecycle } from '@app/framework'
 import { FrameworkApplication } from '@app/framework'
+import { BrowserWindow, Tray, protocol, session } from 'electron'
 import type { ParsedArgs } from 'minimist'
+import fs from 'fs-extra'
+import { rootPath } from './helper/paths'
+import { resolveIconsPath, resolvePages, resolvePreload } from './helper/utils'
 
-export class Application extends FrameworkApplication {
+export class Application extends FrameworkApplication implements Partial<IFrameApplicationLifecycle> {
   static async createApplication(options?: IFrameApplicationConfiguration) {
     return new Application(options)
   }
@@ -10,5 +15,36 @@ export class Application extends FrameworkApplication {
   onBeforeReady(args: ParsedArgs) {
     // eslint-disable-next-line no-console
     console.log(args)
+  }
+
+  onReady(_: ParsedArgs): void {
+    session.defaultSession.setPreloads([
+      resolvePreload('test1'),
+    ])
+
+    protocol.registerStreamProtocol('natmri', (request, cb) => {
+      const url = new URL(request.url)
+      cb(fs.createReadStream(join(rootPath, 'assets', url.hostname, url.pathname)))
+    })
+
+    const win = new BrowserWindow({
+      webPreferences: {
+        sandbox: false,
+        preload: resolvePreload('common'),
+      },
+    })
+
+    win.on('ready-to-show', win.show.bind(win))
+
+    win.loadURL(resolvePages('main'))
+
+    win.webContents.openDevTools({ mode: 'detach' })
+
+    const tray = new Tray(resolveIconsPath())
+
+    tray.displayBalloon({
+      title: 'Balloon',
+      content: 'Balloon Content',
+    })
   }
 }
--- app/electron/main.ts
+++ app/electron/main.ts
@@ -1,25 +1,10 @@
-import { join } from 'path'
-import fs from 'fs'
-import { BrowserWindow, Tray, app, ipcMain, protocol, session } from 'electron'
-import { Emitter, Event } from '@livemoe/utils'
-import { rootPath } from 'helper/paths'
 import { Application } from './application'
-import { resolveIconsPath, resolvePages, resolvePreload } from '~/helper/utils'
 
 Application
   .createApplication({
-
-  })
-  .catch(console.error)
-
-const getSingleInstanceLock = new Emitter<void>()
-const gotSingleInstanceLock = Event.toPromise(getSingleInstanceLock.event)
-
-async function beforeReady() {
-  if (!process.env.PLAYWRIGHT) {
-    protocol.registerSchemesAsPrivileged([
+    protocols: [
       {
-        scheme: 'app',
+        name: 'natmri',
         privileges: {
           secure: true,
           standard: true,
@@ -29,59 +14,7 @@ async function beforeReady() {
           corsEnabled: true,
         },
       },
-    ])
-  }
-}
-
-async function afterReady() {
-  if (!process.env.PLAYWRIGHT) {
-    protocol.registerStreamProtocol('app', (request, cb) => {
-      const url = new URL(request.url)
-      cb(fs.createReadStream(join(rootPath, 'assets', url.hostname, url.pathname)))
-    })
-  }
-
-  // eslint-disable-next-line no-console
-  ipcMain.on('sayHello', (e, message) => console.log(e.sender.id, message))
-
-  session.defaultSession.setPreloads([
-    resolvePreload('test1'),
-  ])
-
-  const win = new BrowserWindow({
-    webPreferences: {
-      sandbox: false,
-      preload: resolvePreload('common'),
-    },
+    ],
   })
-
-  win.on('ready-to-show', win.show.bind(win))
-
-  win.loadURL(resolvePages('main'))
-
-  win.webContents.openDevTools({ mode: 'detach' })
-
-  const tray = new Tray(resolveIconsPath())
-
-  tray.displayBalloon({
-    title: 'Balloon',
-    content: 'Balloon Content',
-  })
-}
-
-gotSingleInstanceLock
-  .then(beforeReady)
-  .then(app.whenReady)
-  .then(afterReady)
   .catch(console.error)
-  .catch(process.exit.bind(process, -1))
-
-if (!app.hasSingleInstanceLock()) {
-  const singleInstanceLock = app.requestSingleInstanceLock()
-
-  if (singleInstanceLock)
-    getSingleInstanceLock.fire()
-  else
-    app.quit()
-}
 
