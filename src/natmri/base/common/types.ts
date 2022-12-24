6572f52892a9b1c7ec351885aaadfcd9e05e524d
starknt <1431880400@qq.com>
2022-12-24 22:01:55 +0800

    chore: add some type function

--- src/natmri/base/common/types.ts
+++ src/natmri/base/common/types.ts
@@ -1,31 +1,53 @@
-export function isUndefined(x: any): x is undefined {
-  return typeof x === 'undefined'
+export function isUndefined(obj: unknown): obj is undefined {
+  return typeof obj === 'undefined'
 }
 
-export function isNull(x: any): x is null {
-  return x === null
+export function isUndefinedOrNull(obj: unknown): obj is undefined | null {
+  return isUndefined(obj) || obj === null
 }
 
-export function isNil(x: any): x is null | undefined {
-  return typeof x === 'undefined'
+export function isFunction(obj: unknown): obj is Function {
+  return typeof obj === 'function'
 }
 
-export function isObject(x: any): x is Record<string, any> {
-  return !isNil(x) && typeof x === 'object'
+export function isDefined<T>(arg: T | undefined | null): arg is T {
+  return !isUndefinedOrNull(arg)
 }
 
-export function isSymbol(x: any): x is symbol {
-  return typeof x === 'symbol'
+export function assertType(condition: unknown, type?: string): asserts condition {
+  if (!condition)
+    throw new Error(type ? `Unexpected type, expected '${type}'` : 'Unexpected type')
 }
 
-export function isPromise(x: any): x is Promise<any> {
-  return !isNil(x) && typeof x === 'object' && typeof x.then === 'function' && typeof x.catch === 'function'
+/**
+ * Converts null to undefined, passes all other values through.
+ */
+export function withNullAsUndefined<T>(x: T | null): T | undefined {
+  return x === null ? undefined : x
 }
 
-export function isBoolean(x: any): x is boolean {
-  return typeof x === 'boolean'
+/**
+ * Converts undefined to null, passes all other values through.
+ */
+export function withUndefinedAsNull<T>(x: T | undefined): T | null {
+  return typeof x === 'undefined' ? null : x
 }
 
-export function isInvalidate(x: any) {
-  return !isNil(x) && !!x
+export function isArray(obj: unknown): obj is unknown[] {
+  return Array.isArray(obj)
+}
+
+/**
+ * @returns whether the provided parameter is of type `object` but **not**
+ *  `null`, an `array`, a `regexp`, nor a `date`.
+ */
+export function isObject(obj: unknown): obj is Object {
+  // The method can't do a type cast since there are type (like strings) which
+  // are subclasses of any put not positvely matched by the function. Hence type
+  // narrowing results in wrong results.
+  return typeof obj === 'object'
+    && obj !== null
+    && !Array.isArray(obj)
+    && !(obj instanceof RegExp)
+    && !(obj instanceof Date)
 }
