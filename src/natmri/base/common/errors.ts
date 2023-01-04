feb580f881dacc11e375d8c296054dab1010f1fc
starknt <1431880400@qq.com>
2023-01-04 10:00:11 +0800

    feat: setup error handler

--- src/natmri/base/common/errors.ts
+++ src/natmri/base/common/errors.ts
@@ -2,7 +2,6 @@ export function toErrorMessage(message: Error, verbose = false): string {
   if (message instanceof Error) {
     if (verbose)
       return `${message.message}\n${message.stack}`
-
     else
       return `${message.message}`
   }
@@ -37,4 +36,81 @@ export function illegalState(name?: string): Error {
     return new Error(`Illegal state: ${name}`)
   else
     return new Error('Illegal state')
+}
+
+export interface ErrorListenerCallback {
+  (error: any): void
+}
+
+export interface ErrorListenerUnbind {
+  (): void
+}
+
+// Avoid circular dependency on EventEmitter by implementing a subset of the interface.
+export class ErrorHandler {
+  private unexpectedErrorHandler: (e: any) => void
+  private listeners: ErrorListenerCallback[]
+
+  constructor() {
+    this.listeners = []
+
+    this.unexpectedErrorHandler = function (e: any) {
+      setTimeout(() => {
+        if (e.stack)
+          throw new Error(`${e.message}\n\n${e.stack}`)
+
+        throw e
+      }, 0)
+    }
+  }
+
+  addListener(listener: ErrorListenerCallback): ErrorListenerUnbind {
+    this.listeners.push(listener)
+
+    return () => {
+      this._removeListener(listener)
+    }
+  }
+
+  private emit(e: any): void {
+    this.listeners.forEach((listener) => {
+      listener(e)
+    })
+  }
+
+  private _removeListener(listener: ErrorListenerCallback): void {
+    this.listeners.splice(this.listeners.indexOf(listener), 1)
+  }
+
+  setUnexpectedErrorHandler(newUnexpectedErrorHandler: (e: any) => void): void {
+    this.unexpectedErrorHandler = newUnexpectedErrorHandler
+  }
+
+  getUnexpectedErrorHandler(): (e: any) => void {
+    return this.unexpectedErrorHandler
+  }
+
+  onUnexpectedError(e: any): void {
+    this.unexpectedErrorHandler(e)
+    this.emit(e)
+  }
+
+  // For external errors, we don't want the listeners to be called
+  onUnexpectedExternalError(e: any): void {
+    this.unexpectedErrorHandler(e)
+  }
+}
+
+export const errorHandler = new ErrorHandler()
+
+export function setUnexpectedErrorHandler(newUnexpectedErrorHandler: (e: any) => void): void {
+  errorHandler.setUnexpectedErrorHandler(newUnexpectedErrorHandler)
+}
+
+export function onUnexpectedError(e: any): undefined {
+  // ignore errors from cancelled promises
+  if (!isCancellationError(e))
+    errorHandler.onUnexpectedError(e)
+
+  return undefined
 }
--- src/natmri/browser-store/electron-main/application.ts
+++ src/natmri/browser-store/electron-main/application.ts
@@ -1,67 +1,122 @@
-import { join } from 'path'
-import type { BrowserWindowConstructorOptions } from 'electron'
-import { BrowserWindow } from 'electron'
-import { runWhenIdle } from 'natmri/base/common/async'
-import { InstantiationService, ServiceCollection, SyncDescriptor } from 'natmri/base/common/instantiation'
+import { IInstantiationService, InstantiationService, ServiceCollection, SyncDescriptor } from 'natmri/base/common/instantiation'
+import { Disposable, DisposableStore } from 'natmri/base/common/lifecycle'
 import { INativeEnvironmentService } from 'natmri/platform/environment/common/environment'
 import { NativeEnvironmentMainService } from 'natmri/platform/environment/electron-main/environmentMainService'
-import { ILifecycleMainService, LifecycleMainService } from 'natmri/platform/lifecycle/electron-main/lifecycleMainService'
+import { ILifecycleMainService, LifecycleMainService, ShutdownReason } from 'natmri/platform/lifecycle/electron-main/lifecycleMainService'
 import { ILoggerService } from 'natmri/platform/log/common/log'
 import { LoggerMainService } from 'natmri/platform/log/node/logMainService'
-import { IProtocolService, ProtocolMainService } from 'natmri/platform/protocol/electron-main/protocolMainService'
+import { IProtocolMainService, ProtocolMainService } from 'natmri/platform/protocol/electron-main/protocolMainService'
 import { INativeTrayMainService, NativeTrayMainService } from 'natmri/platform/tray/electron-main/nativeTrayMainService'
+import { onUnexpectedError, setUnexpectedErrorHandler } from 'natmri/base/common/errors'
+import { Server as ElectronIPCServer } from 'natmri/base/parts/ipc/electron-main/ipc.electron'
+import type { ServicesAccessor } from 'natmri/base/common/instantiation'
+import { BrowserWindow } from 'electron'
 
-export class Application {
+export class Application extends Disposable {
   constructor(
-    @INativeTrayMainService private readonly nativeTrayService: INativeTrayMainService,
+    @IInstantiationService private readonly mainInstantiationService: IInstantiationService,
     @ILoggerService private readonly logService: ILoggerService,
-    @IProtocolService private readonly protocolService: ProtocolMainService,
-    @INativeEnvironmentService private readonly nativeEnvironment: INativeEnvironmentService,
+    @INativeEnvironmentService private readonly environemtService: INativeEnvironmentService,
     @ILifecycleMainService private readonly lifecycleService: ILifecycleMainService,
   ) {
-    runWhenIdle(() => this.startup())
+    super()
+
+    this.registerListeners()
+  }
+
+  async startup() {
+    this.logService.debug('Starting Natmri!')
+
+    const win = new BrowserWindow()
+    win.webContents.openDevTools({ mode: 'detach' })
+    win.loadURL(this.environemtService.getPagesPath('natmri/browser-store/electron-browser/natmri/index.html'))
+
+    // Main process server (electron IPC based)
+    const mainProcessElectronServer = new ElectronIPCServer()
+    this.lifecycleService.onWillShutdown((e) => {
+      if (e.reason === ShutdownReason.KILL) {
+        // When we go down abnormally, make sure to free up
+        // any IPC we accept from other windows to reduce
+        // the chance of doing work after we go down. Kill
+        // is special in that it does not orderly shutdown
+        // windows.
+        mainProcessElectronServer.dispose()
+      }
+    })
+  }
+
+  private registerListeners(): void {
+    // We handle uncaught exceptions here to prevent electron from opening a dialog to the user
+    setUnexpectedErrorHandler(error => this.onUnexpectedError(error))
+    process.on('uncaughtException', error => onUnexpectedError(error))
+    process.on('unhandledRejection', (reason: unknown) => onUnexpectedError(reason))
+
+    // Dispose on shutdown
+    this.lifecycleService.onWillShutdown(() => this.dispose())
+  }
+
+  private onUnexpectedError(error: Error) {
+    if (error) {
+      this.logService.error(`[uncaught exception in main]: ${error}`)
+      if (error.stack)
+        this.logService.error(error.stack)
+    }
   }
 
   static async createApplication() {
-    const [instantianService] = this.createServices()
+    const [instantianService, services] = this.createServices()
 
-    return instantianService.createInstance(new SyncDescriptor(Application))
-  }
-
-  private static createServices(): [InstantiationService] {
-    const services = new ServiceCollection()
-
-    services.set(INativeEnvironmentService, new SyncDescriptor(NativeEnvironmentMainService))
-    services.set(ILoggerService, new SyncDescriptor(LoggerMainService))
+    // Tray
     services.set(INativeTrayMainService, new SyncDescriptor(NativeTrayMainService, [], false))
-    services.set(ILifecycleMainService, new SyncDescriptor(LifecycleMainService))
-    services.set(IProtocolService, new SyncDescriptor(ProtocolMainService, [], false))
 
-    return [new InstantiationService(services, true)]
+    try {
+      await instantianService.invokeFunction(() => {
+        return (instantianService.createInstance(new SyncDescriptor(Application)) as Application).startup()
+      })
+    }
+    catch (error) {
+      instantianService.invokeFunction(this.quit, error)
+    }
   }
 
-  startup() {
-    const options: BrowserWindowConstructorOptions = {
-      frame: false,
-      titleBarStyle: 'hidden',
-      titleBarOverlay: {
-        height: 29, // the smallest size of the title bar on windows accounting for the border on windows 11
-        color: '#F8F8F8',
-        symbolColor: '#FFFFFF',
-      },
+  private static createServices(): [InstantiationService, ServiceCollection] {
+    const services = new ServiceCollection()
+    const disposables = new DisposableStore()
+    process.once('exit', () => disposables.dispose())
+
+    // Environment
+    const environmentMainService = new NativeEnvironmentMainService()
+    services.set(INativeEnvironmentService, environmentMainService)
+
+    // Logger
+    const logService = new LoggerMainService()
+    services.set(ILoggerService, logService)
+
+    // Lifecycle
+    services.set(ILifecycleMainService, new SyncDescriptor(LifecycleMainService, undefined, false))
+
+    // Protocol
+    services.set(IProtocolMainService, new ProtocolMainService(environmentMainService, logService))
+
+    return [new InstantiationService(services, true), services]
+  }
+
+  private static quit(accessor: ServicesAccessor, reason?: Error): void {
+    const logService = accessor.get(ILoggerService)
+    const lifecycleMainService = accessor.get(ILifecycleMainService)
+
+    let exitCode = 0
+
+    if (reason) {
+      exitCode = 1 // signal error to the outside
+
+      if (reason.stack)
+        logService.error(reason.stack)
+
+      else
+        logService.error(`Startup error: ${reason.toString()}`)
     }
 
-    const win = new BrowserWindow(options)
-    const WM_INITMENU = 0x0116 // https://docs.microsoft.com/en-us/windows/win32/menurc/wm-initmenu
-    win.hookWindowMessage(WM_INITMENU, () => {
-      win.setEnabled(false)
-      setTimeout(() => {
-        win.setEnabled(true)
-      })
-
-      return 0
-    })
-    win.webContents.openDevTools()
-    win.loadURL(join(this.nativeEnvironment.getPagesPath('browser-store/electron-browser/natmri/index.html')))
+    lifecycleMainService.kill(exitCode)
   }
 }
