19e22ed2a79aeafc5856b0a32e68e163ad003cca
starknt <1431880400@qq.com>
2023-01-05 23:47:28 +0800

    feat: ipc for sandbox

---
+++ src/natmri/base/parts/ipc/electron-browser/ipc.electron.ts
@@ -0,0 +1,28 @@
+import { ipcRenderer } from 'natmri/base/common/globals'
+import { IPCClient } from 'natmri/base/parts/ipc/common/ipc'
+import { Protocol as ElectronProtocol } from 'natmri/base/parts/ipc/common/ipc.electron'
+import { Event } from 'natmri/base/common/event'
+import { VSBuffer } from 'natmri/base/common/buffer'
+import type { IDisposable } from 'natmri/base/common/lifecycle'
+
+export class Client extends IPCClient implements IDisposable {
+  private protocol: ElectronProtocol
+
+  private static createProtocol(): ElectronProtocol {
+    const onMessage = Event.fromNodeEventEmitter<VSBuffer>(ipcRenderer, 'vscode:message', (_, message) => VSBuffer.wrap(message))
+    ipcRenderer.send('vscode:hello')
+
+    return new ElectronProtocol(ipcRenderer, onMessage)
+  }
+
+  constructor(id: string) {
+    const protocol = Client.createProtocol()
+    super(protocol, id)
+
+    this.protocol = protocol
+  }
+
+  override dispose(): void {
+    this.protocol.disconnect()
+  }
+}
