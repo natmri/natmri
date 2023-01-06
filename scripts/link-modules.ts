f6c64a22a43c2ca19d72a2a565468b5f8db28f74
starknt <1431880400@qq.com>
2023-01-06 16:11:13 +0800

    chore: remove `consola` deps

--- scripts/link-modules.ts
+++ scripts/link-modules.ts
@@ -1,27 +1,32 @@
 import fs, { promises as fsp } from 'fs'
-import consola from 'consola'
 import { appModulesPath, appPackagePath, srcElectronModulesPath, srcElectronPackagePath } from './utils'
 
 // if dependencies is {}, pnpm can't generate `node_modules`
 if (!fs.existsSync(appModulesPath))
   fs.mkdirSync(appModulesPath)
 
-async function linkModules() {
-  return await fsp.symlink(appModulesPath, srcElectronModulesPath, 'junction')
+async function linkModules(err?: Error) {
+  await fsp.symlink(appModulesPath, srcElectronModulesPath, 'junction')
+
+  if (err)
+    throw err
 }
 
-async function linkPackageFile() {
-  return await fsp.symlink(appPackagePath, srcElectronPackagePath, 'file')
+async function linkPackageFile(err?: Error) {
+  await fsp.symlink(appPackagePath, srcElectronPackagePath, 'file')
+
+  if (err)
+    throw err
 }
 
 fsp.lstat(srcElectronModulesPath)
   .then(stat => stat.isSymbolicLink())
   .then(v => !v && linkModules())
   .catch(linkModules)
-  .catch(consola.error)
+  .catch(console.error)
 
 fsp.lstat(srcElectronPackagePath)
   .then(stat => stat.isSymbolicLink())
   .then(v => !v && linkPackageFile())
   .catch(linkPackageFile)
-  .catch(consola.error)
+  .catch(console.error)
