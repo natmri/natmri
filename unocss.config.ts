529cc47854c2e0cc48bce84eaed4ec46457c5abd
starknt <1431880400@qq.com>
2023-01-11 20:05:12 +0800

    chore: add uno rule for electron

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,6 +2,18 @@ import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
 export default defineConfig({
+  rules: [
+    ['region-drag', {
+      '-webkit-app-region': 'drag',
+    }],
+    ['region-no', {
+      '-webkit-app-region': 'no-drag',
+    }],
+  ],
+  shortcuts: {
+    'nti-btn': 'py-1.5 px-4 font-semibold rounded-lg shadow-md',
+    'nti-btn-green': 'text-white bg-green-500 hover:bg-green-700',
+  },
   presets: [
     presetUno(),
     presetAttributify(),
