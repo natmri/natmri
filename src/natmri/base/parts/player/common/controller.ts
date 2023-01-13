41e4a6f6a86ff744faaebf1d8bd1f399365bdca7
starknt <1431880400@qq.com>
2023-01-13 17:33:26 +0800

    chore: delete `player` parts

--- src/natmri/base/parts/player/common/controller.ts
+++
@@ -1,9 +0,0 @@
-import type { IPlayerController } from 'typings/player'
-
-export abstract class PlayerController implements IPlayerController {
-  play(): void {
-  }
-
-  pause(): void {
-  }
-}
--- src/natmri/base/parts/player/common/player.ts
+++
@@ -1,8 +0,0 @@
-import type { IPlayer, IPlayerController, IPlayerUI } from 'typings/player'
-import type { PlayerLoader } from './loader'
-
-export abstract class Player<T> implements IPlayer<T> {
-  abstract loader: PlayerLoader<T>
-  abstract controller: IPlayerController
-  abstract ui: IPlayerUI
-}
