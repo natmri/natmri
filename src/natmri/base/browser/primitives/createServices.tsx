349ecc4ad98abccdf67f49748cfc5da5da0bc92e
starknt <1431880400@qq.com>
2023-01-15 22:08:09 +0800

    chore: cache service

--- src/natmri/base/browser/primitives/createServices.tsx
+++ src/natmri/base/browser/primitives/createServices.tsx
@@ -4,11 +4,18 @@ import { INativeHostService } from 'natmri/platform/native/electron-browser/nati
 import { NativeHostService } from 'natmri/platform/native/electron-browser/nativeHostService'
 import type { ServiceIdentifier } from 'natmri/base/common/instantiation'
 
+const _serviceMap = new Map<ServiceIdentifier<any>, unknown>()
+
 const services = new ServiceCollection()
 services.set(IMainProcessService, new SyncDescriptor(MainProcessService))
 services.set(INativeHostService, new SyncDescriptor(NativeHostService))
 const instantiationService = new InstantiationService(services, true)
 
 export function createService<T>(serviceIdentifier: ServiceIdentifier<T>) {
-  return instantiationService.invokeFunction(accessor => accessor.get(serviceIdentifier))
+  if (_serviceMap.has(serviceIdentifier))
+    return _serviceMap.get(serviceIdentifier) as T
+
+  const service = instantiationService.invokeFunction(accessor => accessor.get(serviceIdentifier))
+  _serviceMap.set(serviceIdentifier, service)
+  return service
 }
