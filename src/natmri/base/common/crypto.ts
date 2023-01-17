36eaf468e05f46df31434ec679e7a7e9d300a303
starknt <1431880400@qq.com>
2023-01-17 22:20:49 +0800

    chore: fix error in main

--- src/natmri/base/common/crypto.ts
+++ src/natmri/base/common/crypto.ts
@@ -1,6 +1,8 @@
+import { isWeb } from 'natmri/base/common/environment'
+
 export function randomUUID(): string {
-  if (typeof require !== 'undefined')
-    return window.require('node:crypto').webcrypto.randomUUID()
+  if (typeof require !== 'undefined' || !isWeb)
+    return require('node:crypto').webcrypto.randomUUID()
 
   return crypto.randomUUID()
 }
