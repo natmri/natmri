f0d1b7031820c349c0014d92c53dfa02e6918cd3
starknt <1431880400@qq.com>
2023-03-09 23:50:48 +0800

    chore: upgrade deps

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,13 +1,7 @@
 import path from 'node:path'
 import fsp from 'node:fs/promises'
-import _rimraf from 'rimraf'
+import { existsSync } from 'node:fs'
 import yaml from 'yaml'
-import { splitVendorChunkPlugin } from 'vite'
-import Solid from 'vite-plugin-solid'
-import UnoCSS from 'unocss/vite'
-import ViteElectronPlugin from 'eevi'
-import { ElectronRendererPlugin } from '@eevi/elexpose/vite'
-import type { PluginOption } from 'vite'
 
 // ------------------------------------------------- //
 // -------- Common Path for Application ------------ //
@@ -30,23 +24,6 @@ export const outputModulePath = path.join(outputAppPath, 'node_modules')
 export const IS_DEV = !!process.env.NATMRI_DEV
 export const IS_TEST = !!process.env.NATMRI_TEST
 
-// ------------------------------------------------- //
-// ------------- Vite Plugin Array ----------------- //
-// ------------------------------------------------- //
-export const VITE_PLUGINS: PluginOption[] = [
-  IS_TEST
-    ? []
-    : [
-        Solid(),
-        UnoCSS(),
-        ViteElectronPlugin(),
-        ElectronRendererPlugin([
-          'wallpaper',
-        ])],
-  splitVendorChunkPlugin(),
-]
-
-// ------------------------------------------------- //
 // ---------- Package Mate Infomation -------------- //
 // ------------------------------------------------- //
 export const MATE = {
@@ -162,17 +139,17 @@ export const resolve: ResolveOptions = {
 }
 
 export async function rimraf(path: string): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    _rimraf(path, (error: any) => {
-      if (error) {
-        console.error(`\x1B[91m\x1B[1m Remove ${path} failed \x1B[0m, error: ${error}`)
-        reject(error)
-      }
+  try {
+    if (!existsSync(path))
+      return
 
-      console.log(`\x1B[91m Remove\x1B[0m\x1B[96m\x1B[1m ${path}\x1B[0m successfully`)
-      resolve()
-    })
-  })
+    await fsp.rm(path, { recursive: true, force: true })
+
+    console.log(`\x1B[91m Remove\x1B[0m\x1B[96m\x1B[1m ${path}\x1B[0m successfully`)
+  }
+  catch (error) {
+    console.error(`\x1B[91m\x1B[1m Remove ${path} failed \x1B[0m, error: ${error}`)
+  }
 }
 
 export async function rimrafTasks(paths: string[]) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { join } from 'node:path'
 import { defineConfig } from 'vitest/config'
-import { outputDistPath as outDir, VITE_PLUGINS as plugins, resolve, srcPath as root, rootPath, setupDevelopmentEnvironment } from './scripts/utils'
+import { splitVendorChunkPlugin } from 'vite'
+import { ElectronRendererPlugin } from '@eevi/elexpose/vite'
+import Solid from 'vite-plugin-solid'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetIcons, presetTypography, presetUno, transformerAttributifyJsx, transformerDirectives } from 'unocss'
+import ViteElectronPlugin from 'eevi'
+import { IS_TEST, outputDistPath as outDir, resolve, srcPath as root, rootPath, setupDevelopmentEnvironment } from './scripts/utils'
 
 const NATMRI_ROOT = join(root, 'natmri')
 const NATMRI_STORE = join(NATMRI_ROOT, 'store')
@@ -19,7 +25,36 @@ export default defineConfig({
   json: {
     stringify: true,
   },
-  plugins,
+  plugins: [
+    ...IS_TEST
+      ? []
+      : [
+          Solid(),
+          UnoCSS({
+            rules: [
+              ['region-drag', { '-webkit-app-region': 'drag' }],
+              ['region-no', { '-webkit-app-region': 'no-drag' }],
+            ],
+            shortcuts: {
+              'n-btn': 'px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2',
+            },
+            presets: [
+              presetUno(),
+              presetTypography(),
+              presetAttributify(),
+              presetIcons(),
+            ],
+            transformers: [
+              transformerDirectives(),
+              transformerAttributifyJsx(),
+            ],
+          }),
+          ViteElectronPlugin(),
+          ElectronRendererPlugin([
+            'wallpaper',
+          ])],
+    splitVendorChunkPlugin(),
+  ],
   build: {
     target: 'esnext',
     assetsDir: 'natmri/assets',
