759f3a62f1035567d8bb33552362ee623e633162
starknt <1431880400@qq.com>
2023-03-29 21:18:45 +0800

    fix: compatible wite preload environment

--- src/natmri/base/common/crypto.ts
+++ src/natmri/base/common/crypto.ts
@@ -1,6 +1,9 @@
 import { isWeb } from 'natmri/base/common/environment'
 
 export function randomUUID(): string {
+  if (typeof window !== 'undefined' && typeof require === 'function')
+    return window.crypto.randomUUID()
+
   if (typeof require !== 'undefined' || !isWeb)
     return require('node:crypto').webcrypto.randomUUID()
 
--- src/natmri/base/common/globals.ts
+++ src/natmri/base/common/globals.ts
@@ -1,14 +1,57 @@
-import type { IpcRenderer } from 'typings/electron'
+import type { IIpcMessagePort, IpcRenderer } from 'typings/electron'
+import type { IpcRendererEvent } from 'electron'
 import type { Process } from 'typings/process'
 
 declare let safeProcess: Process
 declare let safeIpcRenderer: IpcRenderer
-
-// native environment
-if (typeof global !== 'undefined' && typeof window === 'undefined') {
-  global.safeProcess = global.process
-  global.safeIpcRenderer = require('electron').ipcRenderer
+declare let safeIpcMessagePort: IIpcMessagePort
+const _globals: any = {
+  init: false,
 }
 
-export const process = safeProcess
-export const ipcRenderer = safeIpcRenderer
+// native environment
+if (typeof global !== 'undefined' && typeof window === 'undefined' && !_globals.init) {
+  _globals.safeProcess = global.process
+  _globals.safeIpcRenderer = require('electron').ipcRenderer
+  _globals.safeIpcMessagePort = undefined
+  _globals.init = true
+}
+
+// preload environment
+if (typeof navigator !== 'undefined' && !_globals.init) {
+  // @ts-expect-error global variable
+  _globals.safeProcess = process
+  const ipcRenderer = require('electron').ipcRenderer
+  _globals.safeIpcRenderer = ipcRenderer
+  _globals.safeIpcMessagePort = {
+    acquire(responseChannel: string, nonce: string) {
+      const responseListener = (e: IpcRendererEvent, responseNonce: string) => {
+        // validate that the nonce from the response is the same
+        // as when requested. and if so, use `postMessage` to
+        // send the `MessagePort` safely over, even when context
+        // isolation is enabled
+        if (nonce === responseNonce) {
+          ipcRenderer.off(responseChannel, responseListener)
+          window.postMessage(nonce, '*', e.ports)
+        }
+      }
+
+      // handle reply from main
+      ipcRenderer.on(responseChannel, responseListener)
+    },
+  }
+
+  _globals.init = true
+}
+
+// web environment
+if (!_globals.init) {
+  _globals.safeProcess = safeProcess
+  _globals.safeIpcRenderer = safeIpcRenderer
+  _globals.safeIpcMessagePort = safeIpcMessagePort
+  _globals.init = true
+}
+
+export const process: Process = _globals.safeProcess
+export const ipcRenderer: IpcRenderer = _globals.safeIpcRenderer
+export const ipcMessagePort: IIpcMessagePort = _globals.safeIpcMessagePort
