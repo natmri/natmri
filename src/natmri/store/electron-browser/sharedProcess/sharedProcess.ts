7d717b5992128ee45a3ca68d9a6a4b43807cbade
starknt <1431880400@qq.com>
2023-03-30 21:10:36 +0800

    improve: support error handler

--- src/natmri/store/electron-browser/sharedProcess/sharedProcess.ts
+++ src/natmri/store/electron-browser/sharedProcess/sharedProcess.ts
@@ -1,13 +1,15 @@
 import { ipcRenderer } from 'natmri/base/common/globals'
 import { Disposable } from 'natmri/base/common/lifecycle'
 import { Server as MessagePortServer } from 'natmri/base/parts/ipc/electron-browser/ipc.mp'
-import { StaticRouter } from 'natmri/base/parts/ipc/common/ipc'
+import { ProxyChannel, StaticRouter } from 'natmri/base/parts/ipc/common/ipc'
 import { InstantiationService, ServiceCollection, SyncDescriptor } from 'natmri/base/common/instantiation'
 import { ILoggerService } from 'natmri/platform/log/common/log'
 import { LoggerService } from 'natmri/platform/log/browser/logService'
 import { IMainProcessService, MainProcessService } from 'natmri/platform/ipc/common/mainProcessService'
+import { INativeHostService } from 'natmri/platform/native/electron-browser/native'
+import { onUnexpectedError, setUnexpectedErrorHandler, toErrorMessage } from 'natmri/base/common/errors'
+import type { IInstantiationService, ServicesAccessor } from 'natmri/base/common/instantiation'
 import type { IPCServer } from 'natmri/base/parts/ipc/common/ipc'
-import type { ServicesAccessor } from 'natmri/base/common/instantiation'
 
 class SharedProcess extends Disposable {
   private readonly server: IPCServer = this._register(new MessagePortServer())
@@ -30,16 +32,24 @@ class SharedProcess extends Disposable {
     }
 
     process.once('exit', onExit)
-    ipcRenderer.once('vscode:electron-main->shared-process=exit', onExit)
+    ipcRenderer.once('natmri:electron-main->shared-process=exit', onExit)
   }
 
   async initialize() {
-    const mainInstantionService = this.initServices()
+    const mainInstantionService = await this.initServices()
 
-    mainInstantionService.invokeFunction(accessor => this.initChannels(accessor))
+    mainInstantionService.invokeFunction((accessor) => {
+      const logService = accessor.get(ILoggerService)
+
+      // Channels
+      this.initChannels(accessor)
+
+      // Error handlers
+      this.registerErrorHandler(logService)
+    })
   }
 
-  private initServices() {
+  private async initServices(): Promise<IInstantiationService> {
     const services = new ServiceCollection()
 
     // main process
@@ -53,22 +63,45 @@ class SharedProcess extends Disposable {
     // logger
     services.set(ILoggerService, new SyncDescriptor(LoggerService))
 
+    // Native Host
+    const nativeHostService = ProxyChannel.toService<INativeHostService>(mainProcessService.getChannel('nativeHost'))
+    services.set(INativeHostService, nativeHostService)
+
     return new InstantiationService(services)
   }
 
   private initChannels(accessor: ServicesAccessor) {
 
   }
+
+  private registerErrorHandler(logService: ILoggerService): void {
+    window.addEventListener('unhandledrejection', (event: any) => {
+      // See https://developer.mozilla.org/en-US/docs/Web/API/PromiseRejectionEvent
+      onUnexpectedError(event.reason)
+
+      // Prevent the printing of this event to the console
+      event.preventDefault()
+    })
+
+    // Install handler for unexpected errors
+    setUnexpectedErrorHandler((error) => {
+      const message = toErrorMessage(error, true)
+      if (!message)
+        return
+
+      logService.error(`[uncaught exception in sharedProcess]: ${message}`)
+    })
+  }
 }
 
 async function main() {
   const sharedProcess = new SharedProcess()
 
-  ipcRenderer.send('vscode:shared-process->electron-main=ipc-ready')
+  ipcRenderer.send('natmri:shared-process->electron-main=ipc-ready')
 
   await sharedProcess.initialize()
 
-  ipcRenderer.send('vscode:shared-process->electron-main=init-done')
+  ipcRenderer.send('natmri:shared-process->electron-main=init-done')
 }
 
 await main()
